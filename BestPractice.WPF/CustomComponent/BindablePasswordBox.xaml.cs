0be5d8e23bb1a3356a00104c0bb96a61af4156a0
mano1997max <agomaa528.ag@gmail.com>
2021-02-25 17:23:46 +0200

    new bindablePasswordBox Component implementation

---
+++ BestPractice.WPF/CustomComponent/BindablePasswordBox.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace SimpleTrader.WPF.CustomComponent
+{
+	/// <summary>
+	/// Interaction logic for BindablePasswordBox.xaml
+	/// </summary>
+	public partial class BindablePasswordBox : UserControl
+	{
+		public static readonly DependencyProperty PasswordProperty =
+			DependencyProperty.Register("Password",
+				typeof(string),
+				typeof(BindablePasswordBox),
+				new FrameworkPropertyMetadata(string.Empty,
+					FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+					(d,e)=>{},
+					null,
+					false,
+					UpdateSourceTrigger.PropertyChanged
+					)
+				);
+
+		public string Password
+		{
+			get => (string)GetValue(PasswordProperty);
+			set => SetValue(PasswordProperty, value);
+		}
+
+		public BindablePasswordBox()
+		{
+			InitializeComponent();
+		}
+
+		private void Pb_PasswordChanged(object sender, RoutedEventArgs e)
+		{
+			Password = Pb.Password;
+		}
+	}
+}

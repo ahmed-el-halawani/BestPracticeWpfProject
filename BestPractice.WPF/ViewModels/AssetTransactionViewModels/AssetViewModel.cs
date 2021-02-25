d2fa4a15bb911a38e1a43c6d91222c5aaf7563de
mano1997max <agomaa528.ag@gmail.com>
2021-02-25 13:49:46 +0200

    finish asset summary view

--- BestPractice.WPF/ViewModels/AssetTransactionViewModels/AssetViewModel.cs
+++ BestPractice.WPF/ViewModels/AssetTransactionViewModels/AssetViewModel.cs
@@ -1,6 +1,6 @@
 ﻿namespace SimpleTrader.WPF.ViewModels.AssetTransactionViewModels
 {
-	public class AssetViewModel
+	public class AssetViewModel : ViewModelsBase
 	{
 		public string Symbol { get; set; }
 		public int Shares { get; set; }
